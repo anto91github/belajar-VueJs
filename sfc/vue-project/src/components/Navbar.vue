6f1cf6d2356cad4678cb8bfc115107ff6bd5a630
anto91github <antoz91@yahoo.com>
2024-03-04 15:05:12 +0700

    navbar

---
+++ sfc/vue-project/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <RouterLink to="/">Dashboard</RouterLink>
+    <RouterLink to="about">About</RouterLink>
+</template>
+
+<style scoped>
+    a{
+        margin-right: 10px;
+        text-decoration: none;
+        color:rgb(64, 64, 223);
+    }
+    a:hover{
+        color: blueviolet;
+    }
+</style>
---
+++ sfc/vue-project/src/views/About.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Ini About</h1>
+</template>
