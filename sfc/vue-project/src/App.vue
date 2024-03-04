fd038d47ead27bebe1647033f28b1e6ea876ac4f
anto91github <antoz91@yahoo.com>
2024-03-04 12:27:44 +0700

    component

--- sfc/vue-project/src/App.vue
+++ sfc/vue-project/src/App.vue
@@ -1,47 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Student @responseName="(value)=> selectedStudent=value" studentName="Andy">
+    ini adalah nilai slot Parent
+  </Student> 
+  <br>
+  <Student @responseName="showData" studentName="Budi"/> <br>
+  <Student @responseName="showData" studentName="Elly"/> <br>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>
+    Student yg di pilih = {{ selectedStudent }}
+  </p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import Student from './components/Student.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  export default {
+    components: {
+      Student
+    },
+    data(){
+      return{
+        selectedStudent:'',
+        testData:''
+    }
+  },
+  methods:{
+    showData(value){
+      this.selectedStudent = value
+    }
   }
 }
-</style>
+</script>
---
+++ sfc/vue-project/src/components/Student.vue
@@ -0,0 +1,21 @@
+<template>
+    <slot>default msg</slot>
+    <br>
+    Hallo student {{ studentName }}
+    <button @click="sendName">Show</button>
+    <br>
+</template>
+
+<script>
+    export default {
+        props:{
+            studentName : String
+        },
+        emits:['responseName'],
+        methods:{
+            sendName(){
+                this.$emit('responseName', this.studentName)
+            }
+        }
+    }
+</script>
